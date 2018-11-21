07588818457095a1e7d7970cfea0a37e55cd98b0
Caroline Wang <clw74@duke.edu>
2018-11-20 23:08:31 -0500

    finished checking cv auc of glm

--- util.R
+++ util.R
@@ -159,11 +159,10 @@ fit_glm_auc <- function(train, setup) {
     res = train_fold %>%
       mutate(model = map2(X,y, ~ glm(.y ~., 
                                      family = binomial(link = 'logit'), 
-                                     data = .x))) 
-    print(res)
+                                     data = .x))) %>%
       # Compute performance statistics
-    res%>%  mutate(auc_train = pmap_dbl(list(X,y,model), ~ pROC::auc(response=..2,predictor=predict(..3,new_data=..1))),
-             auc_test = pmap_dbl(list(X_test,y_test,model), ~ pROC::auc(response=..2,predictor=predict(..3,new_data=..1)))
+      mutate(auc_train = pmap_dbl(list(X,y,model), ~ pROC::auc(response=..2,predictor=predict(..3,type='response',newdata=..1))),
+             auc_test = pmap_dbl(list(X_test,y_test,model), ~ pROC::auc(response=..2,predictor=predict(..3,type='response',newdata=..1)))
       ) %>%
       
       # Record performance fit statistics over folds
@@ -177,4 +176,83 @@ fit_glm_auc <- function(train, setup) {
   mdl_glm = glm(y ~ ., family=binomial(link='logit'), data=train)
   
   return(list(mdl_glm=mdl_glm, performance=performance))
+}
+
+
+fit_svm <- function(formula, train, param) {
+  ###
+  # Cross validates each combination of parameters in param and returns best model
+  # param is a list of svm parameters as vectors
+  # svm parameters are cost, epsilon, and gamma_scale (a scaling factor on the default gamma value)
+  # train is formatted for xgboost input
+  ###
+  
+  
+  param_df = expand.grid(param) # Each row is a set of parameters to be cross validated
+  n_param = nrow(param_df)
+  
+  ## Compute default gamma parameter
+  gamma_default = 1/ncol(train)
+  param_df = param_df %>%
+    mutate(gamma = gamma_scale * gamma_default)
+  
+  ## Make sure only one type parameter
+  if(length(param$type) == 1){
+    
+    if(str_detect(param$type,'regression')){
+      reg_or_class = 'reg'
+    } else if (str_detect(param$type,'classification')) {
+      reg_or_class = 'class'
+    }
+  } else{
+    stop('Can only handle one type parameter')
+  }
+  
+  ## Allocate space for performance statistics (and set seeds)
+  performance = rep(NA,n_param)
+  
+  cat("Training on",n_param,"sets of parameters.\n")
+  
+  ## Loop through the different parameters sets
+  for (i_param in 1:n_param) {
+    
+    mdcv = suppressWarnings(e1071::svm(formula = formula, 
+                                       data = train, 
+                                       type = param$type,
+                                       kernel = 'radial',
+                                       gamma = param_df$gamma[i_param],
+                                       epsilon = param_df$epsilon[i_param],
+                                       cost = param_df$cost[i_param],
+                                       cross = 5,
+                                       scale = TRUE))
+    
+    if(reg_or_class == "reg"){
+      performance[i_param] = mdcv$tot.MSE
+    } else if (reg_or_class == "class") {
+      performance[i_param] = mdcv$tot.accuracy
+    }
+    
+  }
+  
+  ## Train on best parameters using best number of rounds
+  if(reg_or_class == "reg"){
+    i_param_best = which.min(performance)
+  } else if (reg_or_class == "class") {
+    i_param_best = which.max(performance)
+  }
+  
+  print("Best parameters:")
+  print(t(param_df[i_param_best,]))
+  
+  mdl_best = suppressWarnings(e1071::svm(formula = formula, 
+                                         data = train, 
+                                         type = param$type,
+                                         kernel = 'radial',
+                                         gamma = param_df$gamma[i_param_best],
+                                         epsilon = param_df$epsilon[i_param_best],
+                                         cost = param_df$cost[i_param_best],
+                                         cross = 5,
+                                         scale = TRUE))
+  
+  return(mdl_best)
 }
