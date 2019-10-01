eae02313891a61901f34d654891a66ce8a8070f2
Caroline <carolinewang01@gmail.com>
2019-10-01 18:14:22 -0400

    implemented notebook testing out eq odds constrains and calib eq odds constraints

--- broward/models/advanced_models/six_month/stumps.py
+++ broward/models/advanced_models/six_month/stumps.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import KFold, GridSearchCV
 from sklearn.metrics import roc_auc_score
