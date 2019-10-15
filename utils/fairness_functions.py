c67c63e59bce83c8610ec54ba18fd10d11d070a8
Caroline <carolinewang01@gmail.com>
2019-10-15 17:51:07 -0400

    finished making calibration plots for interpretale models and arnold

--- utils/fairness_functions.py
+++ utils/fairness_functions.py
@@ -1,60 +1,11 @@
 import pandas as pd 
-
-from aequitas.group import Group
-from aequitas.bias import Bias
-from aequitas.fairness import Fairness
-from aequitas.plotting import Plot
 from sklearn.metrics import confusion_matrix
 
 from utils.load_settings import load_settings
 
-
 settings = load_settings()
 decoders = settings["decoders"]
 
-# sensitive_attrs = ['sex', 'race']
-
-# ref_groups_dict = {'sex': 'male',
-#                    'race': 'Caucasian'}
-
-def compute_fairness(df: pd.DataFrame,
-                     preds, 
-                     labels) -> pd.DataFrame:
-    """
-    decoders: dictionary of dictionary of decoders 
-    """
-    df.loc[:, "score"] = preds
-    df.loc[:, "label_value"] = labels
-    df['entity_id'] = df['person_id'].map(str) + " " + df["screening_date"].map(str)
-    df = df[["entity_id", 
-             "sex", 
-             "race", 
-             "score", 
-             "label_value"]]
-
-    # decode numeric encodings for cat var
-    for decoder_name, decoder_dict in decoders.items():
-        df = df.replace({decoder_name: decoder_dict})
-
-    g = Group()
-    xtab, _ = g.get_crosstabs(df, attr_cols=sensitive_attrs)
-    # compute bias
-    b = Bias()
-    bdf = b.get_disparity_predefined_groups(xtab,
-                                            original_df=df,
-                                            ref_groups_dict=ref_groups_dict,
-                                            alpha=0.05,
-                                            # check_significance=True,
-                                            # mask_significance=True
-                                            )
-    f = Fairness()
-    fdf = f.get_group_value_fairness(bdf)
-
-    # list results of fairness analysis
-    parity_determinations = f.list_parities(fdf)
-
-    absolute_metrics = g.list_absolute_metrics(xtab)
-    return fdf[['attribute_name', 'attribute_value'] + absolute_metrics + b.list_disparities(fdf) + parity_determinations]
 
 def compute_confusion_matrix_stats(df, preds, labels, protected_variables):
     df.loc[:, "score"] = preds
@@ -125,11 +76,12 @@ def compute_calibration_fairness(df, probs, labels, protected_variables):
             for window in [(0, 0.1), (0.1, 0.2), (0.2, 0.3), (0.3, 0.4), (0.4, 0.5), (0.5, 0.6), (0.6, 0.7), (0.7, 0.8), (0.8, 0.9), (0.9, 1)]:
                 lo = window[0]
                 hi = window[1]
+                # get the individuals who have a predicted prob in the window (which we call their score)
                 predictions = df["score"][(df[var]==value) & (df["score"] >= lo) & (df["score"] < hi)]
                 labels = df["label_value"][(df[var]==value) & (df["score"] >= lo) & (df["score"] < hi)]
                 
+                # compute P(Y = 1 | Score = score, Protected_var = protected_var)
                 prob = labels.sum() / len(labels)     
-            
 
                 rows.append({
                     "Attribute": var,
@@ -171,6 +123,54 @@ def compute_calibration_discrete_score(long_df:pd.DataFrame,
                            .reset_index())
 
     calib_grps.columns = [score_name, 'Attribute Value', 'n_inds_recid', 'total_inds']
-    calib_grps["P(Y = 1 | Score = score)"] =  calib_grps['n_inds_recid'] / calib_grps['total_inds']
+    calib_grps["P(Y = 1 | Score = score, Attr = attr)"] =  calib_grps['n_inds_recid'] / calib_grps['total_inds']
     
+    return calib, calib_grps
+
+
+def parse_calibration_matrix(calibration_matrix: pd.DataFrame, 
+                             problem_name:str, 
+                             score_name:str):
+    
+    calibration_matrix[score_name] = (calibration_matrix["Lower Limit Score"].astype(str) + "-" 
+                                     + calibration_matrix["Upper Limit Score"].astype(str))
+    calibration_matrix.drop(columns = ['Lower Limit Score', 'Upper Limit Score'], inplace=True)
+    calibration_matrix.rename(columns={'Conditional Frequency': "P(Y = 1 | Score = score, Attr = attr, Fold = fold)"}, 
+                              inplace=True)
+    
+    # compute calibration by sensitive attribute (average out the fold)
+    # filter entries where # inds evaluated on is 0
+    calib_grps = (calibration_matrix[calibration_matrix["Individuals Evaluated On"] != 0]
+                            .groupby([score_name, 'Attribute', 'Attribute Value']).apply(lambda x: np.average(x["P(Y = 1 | Score = score, Attr = attr, Fold = fold)"], 
+                                                                         weights=x["Individuals Evaluated On"]))
+                           .reset_index()
+                           .rename(columns={0: "P(Y = 1 | Score = score, Attr = attr)"}))
+
+    # put back the groups where the # inds eval on is 0
+    num_inds = (calibration_matrix[[score_name, 'Attribute', 'Attribute Value', 'Individuals Evaluated On']]
+                .groupby([score_name, 'Attribute', 'Attribute Value'])
+                .sum()
+                .reset_index())
+
+    calib_grps = calib_grps.merge(num_inds, 
+                                  on=[score_name, 'Attribute', 'Attribute Value'],
+                                  how='right')
+
+    # compute overall calib 
+    calib = (calib_grps[calib_grps["Individuals Evaluated On"] != 0].groupby(score_name)
+                       .apply(lambda x: np.average(x["P(Y = 1 | Score = score, Attr = attr)"], 
+                                                   weights=x["Individuals Evaluated On"]))
+                       .reset_index()
+                       .rename(columns={0: 'P(Y = 1 | Score = score)'}))
+
+    # num individuals w/ sensitive attrs per fold 
+    num_inds = (calib_grps[[score_name, 'Individuals Evaluated On']]
+                .groupby([score_name])
+                .sum()
+                .reset_index())
+
+    calib = calib.merge(num_inds, 
+                      on=[score_name],
+                      how='right')
+
     return calib, calib_grps
--- utils/plot_calibration.py
+++ utils/plot_calibration.py
@@ -1,27 +1,26 @@
 import pandas as pd 
+import numpy as np
 import matplotlib.pyplot as plt
 
 from utils.plotting_helpers import safe_save_plt
-from utils.fairness_functions import compute_calibration_discrete_score
 
 
-def plot_calibration_for_score_on_problem(long_df: pd.DataFrame, 
-                                           problem_name: str,
-                                           score_name: str,
-                                           region: str,
-                                           save_path=None):
+def plot_calibration_for_score_on_problem(calib: pd.DataFrame, 
+                                          calib_grps: pd.DataFrame,
+                                          problem_name: str,
+                                          score_name: str,
+                                          region: str,
+                                          xtick_labels=None,
+                                          save_path=None):
     """Plots calibration for risk scores which are not probabilities
     Keyword Arguments:
-        scores: df with columns [score_name, P(Y = 1 | Score = score)]; should contain the probability of 
+        calib: df with columns [score_name, P(Y = 1 | Score = score)]; should contain the probability of 
                 recidivism for each score. 
-        sensitive_attr_scores: df with columns [score_name, Attribute Value, P(Y = 1 | Score = score, Attr=attr)]; 
+        calib_grps: df with columns [score_name, Attribute Value, P(Y = 1 | Score = score, Attr=attr)]; 
                                 should contain the prob of recidivism for each sensitive group and score 
         problem_name: variable name of the prediction problem for this plot 
         score_name: 
     """
-    calib, calib_grps = compute_calibration_discrete_score(long_df=long_df, 
-                                                            problem_name=problem_name, 
-                                                            score_name=score_name)
     
     plt.figure(figsize=(10, 7))
     plt.style.use('ggplot')
@@ -31,32 +30,36 @@ def plot_calibration_for_score_on_problem(long_df: pd.DataFrame,
     plt.plot(calib[score_name], 
              calib['P(Y = 1 | Score = score)'], 
              color='black', marker='o', 
-             linewidth=1, markersize=2)
+             linewidth=1, markersize=2,
+             label='All individuals')
 
     # group level calibration 
     colors=['red', 'green', 'orange', 'maroon', 'royalblue', 'mediumslateblue']
     for i, (name, group_df) in enumerate(calib_grps.groupby("Attribute Value")):
         if name == 'female' or name == 'male':
             plt.plot(group_df[score_name], 
-                     group_df['P(Y = 1 | Score = score)'], 
+                     group_df['P(Y = 1 | Score = score, Attr = attr)'], 
                      color=colors[i], marker='o', linewidth=1, markersize=2,
                      linestyle='--',
                      label=name)
         else:
             plt.plot(group_df[score_name], 
-                     group_df['P(Y = 1 | Score = score)'], 
+                     group_df['P(Y = 1 | Score = score, Attr = attr)'], 
                      color=colors[i], marker='o', linewidth=1, markersize=2,
                      label=name)
 
     # axes settings
-    plt.xlabel(f"\n{score_name} Score", fontsize=20)
+    if xtick_labels is not None:
+        plt.xticks(np.arange(len(xtick_labels)), xtick_labels)
+    plt.xlabel(f"\n{score_name} score", fontsize=20)
 
     plt.ylim(0,1)
     plt.ylabel('P(Y = 1 | Score = score, Attr = attr)\n', fontsize=20)
 
     # Create legend, add title, & show/save graphic
     plt.legend(fontsize=14)
-    plt.title(f'Calibration of {score_name} on \n{problem_name} in {region}', fontsize=24)
+    score_name_formatted = score_name.capitalize()
+    plt.title(f'Calibration of {score_name_formatted} on \n{problem_name} in {region}', fontsize=24)
 
     if save_path is not None: 
         safe_save_plt(plt, save_path)
